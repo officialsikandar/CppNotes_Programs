22ae617078e4b9112b1df4b4565339bff5949b46
Sikandar Khan <84439796+officialsikandar@users.noreply.github.com>
2022-02-01 18:38:03 +0530

    Create Tpreprocessers.cpp

---
+++ Tpreprocessers.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+#define val 12
+
+using namespace std;
+
+int main()
+{
+    cout << "Value is :" << val << endl;
+    // #undef val
+    // int val=23;
+    cout << "Value is :" << val << endl;
+}
+
